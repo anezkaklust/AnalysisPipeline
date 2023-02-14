0c5cd732e672388d1730e21e937cc0b3dc2e0e68
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-02-14 06:52:13 -0600

    Updated 2D pipeline

--- pipeline2D.sh
+++ pipeline2D.sh
@@ -1,68 +1,92 @@
 #!/bin/bash
-
-plottingDir=/minerva/app/users/anezkak/MAT_GitHub/NSFNukeCCInclusive/ana/panel_plottingConcise
-datadDir=/minerva/data2/users/anezkak/2023/
-scriptDir=/minerva/app/users/anezkak/MAT_GitHub/NSFNukeCCInclusive/ana/make_hists/
-
 now=$(date +%m-%d-%Y)
 printf "%s\n" "$now"
+
+plottingDir=/minerva/app/users/anezkak/MAT_GitHub/NSFNukeCCInclusive/ana/panel_plottingConcise/
+#datadDir=/minerva/data/users/anezkak/${now}/2D/
+datadDir=/minerva/data/users/anezkak/02-02-2023/2D/
+scriptDir=/minerva/app/users/anezkak/MAT_GitHub/NSFNukeCCInclusive/ana/make_hists/
+
+mkdir -p ${datadDir}
 
 # for all playlists
 #for playlist in minervame5A minervame6A minervame6B minervame6C minervame6D minervame6E minervame6F minervame6G minervame6H minervame6I minervame6J
 for playlist in minervame6A
 do
-    dirpwd=/pnfs/minerva/persistent/users/anezkak/${playlist}/
+    dirpwd=/pnfs/minerva/persistent/users/anezkak/1D/${playlist}/
+    dirpwd2D=/minerva/app/users/anezkak/MAT_GitHub/NSFNukeCCInclusive/ana/make_hists2D/
     cd ${dirpwd}
     #cp */*.root . 
     cd ${datadDir}
-    savedir="${now}_${playlist}_2D"
+    savedir="${playlist}"
     mkdir -p ${savedir} && cd ${savedir}
     pwd
-
+    #: '
     echo Event Loop plots
+    cd ${datadDir}${savedir}/
     mkdir -p "EventLoop"
     cd "EventLoop"
     cd ${plottingDir}
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 2 26 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 2 82 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 3 26 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 3 06 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 3 82 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 4 82 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 5 26 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 5 82 ${playlist} 
-    #./plotEventSelection ${dirpwd} ${datadDir}${savedir}/EventLoop 99 99 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 2 26 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 2 82 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 26 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 06 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 82 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 4 82 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 5 26 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 5 82 ${playlist} 
+    #./plotEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop 99 99 ${playlist} 
+    #./plotDaisyEventSelection2D ${dirpwd} ${datadDir}${savedir}/EventLoop ${playlist} 
     
     echo Event Loop Fractional Error plots
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 2 26 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 2 82 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 3 26 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 3 06 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 3 82 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 4 82 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 5 26 ${playlist} 
-    #./FractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 5 82 ${playlist} 
-    #./plotFractionalError ${dirpwd} ${datadDir}${savedir}/EventLoop 99 99 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 2 26 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 2 82 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 26 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 06 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 3 82 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 4 82 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 5 26 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 5 82 ${playlist} 
+    #./plotFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop 99 99 ${playlist} 
+    #./plotDaisyFractionalError2D ${dirpwd} ${datadDir}${savedir}/EventLoop ${playlist} 
+    
+    #'
+    echo Migration
     cd ${datadDir}${savedir}/
+    mkdir -p "Migration"
+    cd "Migration"
+    #echo Copy migration files to data
+    #cp ${dirpwd2D}/Migration*.root .
+    echo Migration plots
 
-    #mkdir -p "Migration"
-    #cd "Migration"
-    #echo Migration plots
-    #cd ${plottingDir}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 2 26 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 2 82 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 3 26 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 3 06 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 3 82 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 4 82 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 5 26 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 5 82 ${playlist}
+    python ${plottingDir}plotMigrationMatrix2D.py ${datadDir}${savedir}/Migration 99 99 ${playlist}
+    echo Migration Tracker Daisy
+    python ${plottingDir}plotDaisyMigrationMatrix2D.py ${datadDir}${savedir}/Migration ${playlist}
+
 
     echo Efficiency Numerator Denominator plots
+    cd ${datadDir}${savedir}/
     mkdir -p "Efficiency"
     cd "Efficiency"
     cd ${plottingDir}
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 2 26 ${playlist} 
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 2 82 ${playlist} 
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 3 26 ${playlist} 
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 3 06 ${playlist}
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 3 82 ${playlist}
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 4 82 ${playlist} 
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 5 26 ${playlist}
-    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 5 82 ${playlist} 
-    ./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 99 99 ${playlist}    
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 2 26 ${playlist} 
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 2 82 ${playlist} 
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 3 26 ${playlist} 
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 3 06 ${playlist}
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 3 82 ${playlist}
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 4 82 ${playlist} 
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 5 26 ${playlist}
+    #./plotEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency 5 82 ${playlist} 
+    #./plotEfficiencyNumDenom ${dirpwd} ${datadDir}${savedir}/Efficiency 99 99 ${playlist} 
+    #./plotDaisyEfficiencyNumDenom2D ${dirpwd} ${datadDir}${savedir}/Efficiency ${playlist}    
    
     echo Efficiency
     cd ${datadDir}${savedir}/Efficiency
@@ -74,8 +98,9 @@ do
     #python ${plottingDir}/plotEfficiency2D.py ${dirpwd} 4 82 ${playlist} 
     #python ${plottingDir}/plotEfficiency2D.py ${dirpwd} 5 26 ${playlist} 
     #python ${plottingDir}/plotEfficiency2D.py ${dirpwd} 5 82 ${playlist} 
-    python ${plottingDir}/plotEfficiency2D.py ${dirpwd} 99 99 ${playlist} 
-
+    #python ${plottingDir}/plotEfficiency2D.py ${dirpwd} 99 99 ${playlist} 
+    #python ${plottingDir}/plotDaisyEfficiency2D.py ${dirpwd} ${playlist} 
+    
     echo Plastic sidebands Scale Factors
     #./getScaleFactors_PlasticSB ${dirpwd}/ ${datadDir}/${savedir}/PlastisSidebands 2 26 ${playlist} | tee ${datadDir}/${savedir}/PlastisSidebands/plasticSidebands_t2_z26.txt
     #./getScaleFactors_PlasticSB ${dirpwd}/ ${datadDir}/${savedir}/PlastisSidebands 2 82 ${playlist} | tee ${datadDir}/${savedir}/PlastisSidebands/plasticSidebands_t2_z82.txt
