d29dbfcf99d116195dcd9fd797962d2677cfcf19
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-03-05 12:20:53 -0600

    Added sys arg for output folder for grid submission.

--- submitGrid.sh
+++ submitGrid.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo Event Loop
-source submitEventLoop.sh | tee eventLoopSubmission.txt
+source submitEventLoop.sh 1D | tee eventLoopSubmission.txt
 echo Efficiency
-source submitEfficiency.sh | tee efficiencySubmission.txt
+source submitEfficiency.sh 1D | tee efficiencySubmission.txt
 echo Migration
-source submitMigration.sh | tee migrationSubmission.txt
-echo Migration2D
-source submitMigration2D.sh | tee migrationSubmission2D.txt
+source submitMigration.sh 1D | tee migrationSubmission.txt
+#echo Migration2D
+#source submitMigration2D.sh | tee migrationSubmission2D.txt
 echo Plastic Sidebands
-source submitPlasticSidebands.sh | tee plasticSubmission.txt
+source submitPlasticSidebands.sh 1D | tee plasticSubmission.txt
 
 echo DONE
