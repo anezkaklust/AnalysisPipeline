2ac90e468e944e437553228215bfb89901ba9a75
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-06-07 13:11:30 -0500

    Changed memory to 3GB.

--- SubmitJobsToGrid_MAT_Tutorial2D.py
+++ SubmitJobsToGrid_MAT_Tutorial2D.py
@@ -120,7 +120,7 @@ output_dir = "$CONDOR_DIR_HISTS/"
 if(not os.path.isdir(opts.outdir)):
     os.makedirs(opts.outdir)
 
-memory = 2000
+memory = 3000
 
 # Check the stage is valid or not
 if opts.stage not in valid_stages:
